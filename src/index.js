2065e22d947c80495d5697b6272b9d60de8cc467
valarenuka32 <valarenuka32@gmail.com>
2023-07-24 10:19:21 +0530

    add file

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// express using create server
+// const express = require('express');
+// const fs = require('fs');
+// const app = express();
+
+// app.get('/', function (req, res) {
+//   fs.readFile('text.html', function (err, data) {
+//     res.writeHead(200, { 'content-type': 'test.html' });
+//     res.write(data);
+//     res.end();
+//   });
+// });
+// app.listen(7002);
+
+//http using create server 
+
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function (req, res) {
+  fs.readFile('text.html', function (err, data) {
+    res.writeHead(200, { 'content-type': 'test.html' });
+    res.write(data);
+    return res.end();
+  });
+}).listen(4050);
