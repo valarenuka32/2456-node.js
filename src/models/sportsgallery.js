f23f01c65475b414c7e0a7fe1bc123ab7ccb9390
valarenuka32 <valarenuka32@gmail.com>
2023-09-19 10:55:50 +0530

    add file

--- src/models/sportsgallery.js
+++ src/models/sportsgallery.js
@@ -1,7 +1,22 @@
 const mongoose = require("mongoose");
+
 const sportsgallerySchema = new mongoose.Schema(
     {
-
+        sport_name: {
+            type: String,
+            trim: true,
+        },
+        Description: {
+            type: String,
+            trim: true,
+        },
+        sport_img: {
+            type: String,
+        },
+        is_active: {
+            type: Boolean,
+            default: true,
+        },
     },
     {
         timestamps: true,
