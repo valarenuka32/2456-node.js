bc90bbcb2b1cd7464d66281982c601095de512b8
valarenuka32 <valarenuka32@gmail.com>
2023-09-21 10:39:33 +0530

    add file

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,12 @@ dotenv.config();
 const envVarsSchema = joi.object({
     PORT: joi.number().default(7000),
     MONGODB_URL: joi.string().trim().description("mongoodb url"),
-    BASE_URL:joi.string().trim().description("Base URL"),
+    BASE_URL: joi.string().trim().description("Base URL"),
+    SMTP_HOST: joi.string().description("server that will send the email"),
+    SMTP_PORT: joi.number().description("port to connect the emali"),
+    SMTP_USERNAME: joi.string().description("username for email server "),
+    SMTP_PASSWORD: joi.string().description("password for email server"),
+    EMAIL_FROM: joi.string().description("the form field in the emails sent by the app")
 }).unknown();
 
 const { value: envVars, error } = envVarsSchema
@@ -27,5 +32,16 @@ module.exports = {
             useUnifiedTopology: true,
         },
     },
-    base_url:envVars.BASE_URL,
+    base_url: envVars.BASE_URL,
+    email: {
+        smtp: {
+            host: envVars.SMTP_HOST,
+            port: envVars.SMTP_PORT,
+            auth: {
+                user: envVars.SMTP_USERNAME,
+                pass: envVars.SMTP_PASSWORD,
+            },
+        },
+        from: envVars.EMAIL_FROM,
+    },
 };
