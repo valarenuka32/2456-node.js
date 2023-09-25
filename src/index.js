6ab59d66243cbe9a09f45dd8c12e221ad52ccb46
valarenuka32 <132243973+valarenuka32@users.noreply.github.com>
2023-09-25 10:58:52 +0530

    add file

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const bodyParser = require("body-parser");
+const { connectDB } = require("./db/dbconections");
+
+// database connection
+connectDB()
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+// namespace
+app.use("/v1", routes);
+// server
+const server = http.createServer(app);
+
+server.listen(config.port, () => {
+    console.log(`server is started port no ${config.prot} `);
+});
