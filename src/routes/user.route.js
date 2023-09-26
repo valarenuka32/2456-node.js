6314f59c03429f907c810a427297a668d248f7c9
valarenuka32 <132243973+valarenuka32@users.noreply.github.com>
2023-09-26 14:34:10 +0530

    add file

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,9 @@
-const express=require("express");
-const {uservalidation}=require("../validations");
-const {userController}=require("../controllers");
-const validate=require("../middlewares/validate");
+const express = require("express");
+const { uservalidation } = require("../validations");
+const { userController } = require("../controllers");
+const validate = require("../middlewares/validate");
 
-const router=express.Router();
+const router = express.Router();
 
 // create
 router.post(
@@ -11,5 +11,20 @@ router.post(
     validate(uservalidation.createUser),
     userController.createUser
 );
+// list
+router.get(
+    "/list",
+    userController.userList
+);
+// update
+router.put(
+    "/update-user",
+    userController.updateRecode
+);
+// update
+router.delete(
+    "/delete-user",
+    userController.deleteRecode
+);
 
-module.exports=router;
+module.exports = router;
