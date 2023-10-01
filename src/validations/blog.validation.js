2970ea11d703c582c695234f9df96bfbb4850fd7
valarenuka32 <valarenuka32@gmail.com>
2023-10-01 22:13:26 +0530

    add file

--- src/validations/blog.validation.js
+++ src/validations/blog.validation.js
@@ -5,6 +5,8 @@ const createBlog = {
     body: Joi.object().keys({
         blog_name: Joi.string().trim().required(),
         blog_description: Joi.string().trim().required(),
+        images: Joi.string(),
+        category: Joi.string(),
     })
 };
 
